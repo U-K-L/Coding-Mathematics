bb01bea12f2c8df39995df30a8d776f8004a45ce
Noah Williams <nwilliams5343@students.mc3.edu>
2018-10-03 16:50:12 -0400

    line

--- Introduction/Main.js
+++ Introduction/Main.js
@@ -5,5 +5,10 @@ window.onload = function () {
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight;
 
-    context.fillRect(0, 0, width, height);
+    for(var i = 0; i < 100; i+= 1){
+      context.beginPath();
+      context.moveTo(Math.random() * width, Math.random()*height);
+      context.lineTo(Math.random() * width, Math.random()*height);
+      context.stroke();
+    }
 }
